923bbe914162f83314fb7c4ade10cbceac446b97
-ˏˋ Maple ˊˎ <git@kalitsune.net>
2024-03-20 20:21:01 +0100

    💀 Added a skeleton component

---
+++ src/lib/components/Skeleton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let width: string = '100%';
+	export let height: string = '100%';
+</script>
+
+<div class="skeleton" style:width style:height />
+
+<style>
+	.skeleton {
+		border-radius: var(--radius-3);
+		animation: 1.5s linear infinite alternate blink;
+	}
+
+	@keyframes blink {
+		from {
+			background: var(--surface-2);
+		}
+		to {
+			background: var(--surface-4);
+		}
+	}
+</style>
