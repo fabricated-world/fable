a2baf8d74ecdd7205750b69f765ff9351dc06570
-ˏˋ Maple ˊˎ <git@kalitsune.net>
2024-03-23 00:02:15 +0100

    🔗 Added a new structure for the blog static assets
    with automatic symlinks

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig, loadEnv } from 'vite';
 
 import dynamicImport from 'vite-plugin-dynamic-import';
 import path from 'path';
+import fs from 'fs';
 
 export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
-	console.log(process.env.BLOG_PATH);
+	!fs.existsSync(path.join(__dirname, "static", "blog")) && fs.symlinkSync(path.join(__dirname, process.env.BLOG_STATIC), path.join(__dirname, "static", "blog"), 'junction');
+  
 	return defineConfig({
 		plugins: [sveltekit(), dynamicImport()],
 		ignore: ['**/*.adoc'],
@@ -19,8 +21,9 @@ export default ({ mode }) => {
 		server: {
 			fs: {
 				// Allow serving files from one level up to the project root
-				allow: ['.' + process.env.BLOG_PATH]
+				allow: ['.' + process.env.BLOG_PATH, '.' + process.env.BLOG_STATIC]
 			}
-		}
+		},
+        beforeBuild: [`node blogstatic.sync.js ${ path.join(__dirname, process.env.BLOG_STATIC)}`]
 	});
 };
